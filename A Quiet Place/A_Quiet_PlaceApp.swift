40e504c35811adc3431e8d713cd67676714c672e
ZaraGlynn <zara.glynn.1992@gmail.com>
2023-03-31 09:57:59 +1100

    Initial Commit

---
+++ A Quiet Place/A_Quiet_PlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  A_Quiet_PlaceApp.swift
+//  A Quiet Place
+//
+//  Created by Zara Glynn on 31/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct A_Quiet_PlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A Quiet Place/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  A Quiet Place
+//
+//  Created by Zara Glynn on 31/3/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
